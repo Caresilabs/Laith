67600d52adf629d40cc210ddfd74b2a6000d821e
Simon Bothén <caresilabs@gmail.com>
2015-03-16 17:40:46 +0100

    Test Level :smirk:

---
+++ Unity Project/Laith/Assets/Scripts/Gameplay/Character/BasePlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer : MonoBehaviour {
+	
+	float speed  = 6.0f;
+	float jumpSpeed  = 11.0f;
+	float gravity  = 20.0f;
+	
+	private Vector3 moveDirection = Vector3.zero;
+	private int jumpCount;
+
+	public void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if (controller.isGrounded) {
+			jumpCount = 0;
+		}
+
+		float prevSpeed = moveDirection.y;
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, 0); //Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		moveDirection.y = prevSpeed;
+
+		// Allow double jumping
+		if (Input.GetKeyDown("space") && jumpCount < 2) {
+			moveDirection.y = jumpSpeed;
+			jumpCount++;
+		}
+		
+		// Apply gravity
+		moveDirection.y -= gravity * Time.deltaTime;
+		
+		// Move the controller
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
