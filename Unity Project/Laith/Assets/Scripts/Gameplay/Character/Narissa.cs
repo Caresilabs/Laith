e43880014ef2dc876cf41e9b52c2349b9f3f5228
Simon Bothén <caresilabs@gmail.com>
2015-03-30 15:01:09 +0200

    Networking

--- Unity Project/Laith/Assets/Scripts/Gameplay/Character/Narissa.cs
+++ Unity Project/Laith/Assets/Scripts/Gameplay/Character/Narissa.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Narissa : BasePlayer {
+public class Narissa : BasePlayerController {
 
 	public float hookLength = 100f;
+
+	private SpringJoint hook;
 
 	public override void Update () {
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
 			Hook ();
+		}
+
+		if (hook != null) {
+			Debug.DrawRay (hook.anchor, hook.connectedAnchor - hook.anchor, Color.red, hook.maxDistance);
+
+			//var target = hook.connectedBody.transform.TransformPoint(hook.connectedBody.transform.position);
+			var distance = Vector3.Distance(rigidbody.position, hook.connectedBody.position);
+
+			if (distance < 2) {
+				Destroy(hook);
+				hook = null;
+			}
+
 		}
 
 		base.Update ();
@@ -16,17 +31,25 @@ public class Narissa : BasePlayer {
 	void Hook(){
 		RaycastHit raycastHit;
 		Vector3 mouseDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(rigidbody.transform.position);
+		mouseDirection.Normalize ();
+
 		if(Physics.Raycast(rigidbody.transform.position, mouseDirection, out raycastHit, hookLength)){
+			///if (raycastHit.rigidbody != null && raycastHit.rigidbody.tag == "Terrain") return;
+			if (raycastHit.rigidbody == null) return;
+
 			SpringJoint tempJoint;
-			rigidbody.transform.position = raycastHit.point;
+			//rigidbody.transform.position = raycastHit.point;
 			tempJoint = this.gameObject.AddComponent<SpringJoint>();
 			tempJoint.connectedBody = raycastHit.rigidbody;
-			tempJoint.maxDistance = 0f;
+			tempJoint.maxDistance = 0;
 			tempJoint.minDistance = 0f;
 			tempJoint.spring = 1f;
 			tempJoint.damper = 0f;
+
+
+			hook = tempJoint;
 		}
-		Debug.DrawRay (rigidbody.transform.position, mouseDirection * 100, Color.red, 10f);
+		//Debug.DrawRay (rigidbody.transform.position, mouseDirection * 100, Color.red, 10f);
 			
 	}
 }
