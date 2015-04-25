b450945224924d8202c51ab4b6e24876a4a267e4
Ossibo <card-gamer@hotmail.com>
2015-04-25 23:56:49 +0200

    Destroyable Objects

---
+++ Unity Project/Laith/Assets/Scripts/Level/SoftWall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoftWall : MonoBehaviour {
+
+	//Summary
+	//Class used to destroy a soft object
+	//Author: Simon J
+	//
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider Entity){
+		if (Entity.tag == "Player" && Entity.GetComponent<Gareth>().sprint == true) {
+			Destroy(this.gameObject);
+		}
+	}
+}
