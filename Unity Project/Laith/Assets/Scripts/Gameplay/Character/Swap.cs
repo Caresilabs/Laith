bacb5fa3207bae5fc6993976d2fdd5a1c7dde3b8
Andreas Karlsson <andreas@kodacreations.se>
2015-05-05 00:09:00 +0200

    Swap comments
    Buntade ihop kommentarer i koden så det ser snyggare ut

--- Unity Project/Laith/Assets/Scripts/Gameplay/Character/Swap.cs
+++ Unity Project/Laith/Assets/Scripts/Gameplay/Character/Swap.cs
@@ -11,36 +11,38 @@ public class Swap : MonoBehaviour {
 	public bool isOnCooldown;
 	private float tempCooldown;
 
-	// Use this for initialization
 	void Start () {
 		tempCooldown = cooldown;
 	}
 
-	// Method that switches positions
 	void SwitchPos(){
+		// Finds a game-object with the desired name
 		GameObject target = GameObject.Find("Gareth(Clone)");
 
-		Vector3 tempPosition = transform.position;   			// Stores this objects position
-		Vector3 tempVelocity = rigidbody.velocity; 				// Stores this objects velocity
-		Quaternion tempRotation = transform.rotation;			// Stores this objects rotation
+		// Stores the position, velocity and rotation of this object as temporary variables
+		Vector3 tempPosition = transform.position;
+		Vector3 tempVelocity = rigidbody.velocity;
+		Quaternion tempRotation = transform.rotation;
 
-		transform.position = target.transform.position;   		// Sets this objects position to be the targets position
-		rigidbody.velocity = target.rigidbody.velocity;			// Sets this objects velocity to be the targets velocity
-		transform.rotation = target.transform.rotation;			// Sets this objects rotation to be the targets rotation
+		// Sets this objects position, velocity and rotation to be the targets position, velocity and rotation
+		transform.position = target.transform.position;
+		rigidbody.velocity = target.rigidbody.velocity;
+		transform.rotation = target.transform.rotation;
 
-		target.transform.position = tempPosition;   			// Sets the targets position to be the old position of this object
-		target.rigidbody.velocity = tempVelocity;				// Sets the targets velocity to be the old velocity of this object
-		target.transform.rotation = tempRotation;				// Sets the targets rotation to be the old rotiation of this object
+		// Sets the targets position, velocity and rotation to be the old position, velocity and rotation of this object
+		target.transform.position = tempPosition;
+		target.rigidbody.velocity = tempVelocity;
+		target.transform.rotation = tempRotation;
 
 		isOnCooldown = true;
 	}
 
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Q) && !isOnCooldown){
 			SwitchPos();
 		}
 
+		// Timer for the cooldown
 		if (isOnCooldown) {
 			cooldown -= Time.deltaTime;
 			if (cooldown <= 0){
