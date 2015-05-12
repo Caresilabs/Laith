85a38e0d07d5ac9cfae43b03ff3cc177786e836c
Ossibo <card-gamer@hotmail.com>
2015-05-12 11:45:18 +0200

    Någon sak
    haha

---
+++ Unity Project/Laith/Assets/Scripts/Layer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layer : MonoBehaviour {
+
+	public static int players = 8;
+	public static GameObject[] FindGameObjectsWithLayer (int layer)  {
+		GameObject[] goArray = FindObjectsOfType(GameObject);
+		GameObject goList = new System.Collections.Generic.List.<GameObject>();
+		for (int i = 0; i < goArray.Length; ++i) {
+			if (goArray[i].layer == layer) {
+				goList.Add(goArray[i]);
+			}
+		}
+		if (goList.Count == 0) {
+			return null;
+		}
+		return goList.ToArray();
+	}
+}
