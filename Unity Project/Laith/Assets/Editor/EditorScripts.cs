87de06bab7d3e1524891c1cffa709c975b75a357
Tim Lindstam <tim.sunds@aol.com>
2015-05-12 13:04:44 +0200

    simons synk
    huehuehue

--- Unity Project/Laith/Assets/Editor/EditorScripts.cs
+++ Unity Project/Laith/Assets/Editor/EditorScripts.cs
@@ -8,6 +8,9 @@ class EditorScrips : EditorWindow
 	[MenuItem("Play/PlayMe _%h")]
 	public static void RunMainScene()
 	{
+		if (EditorApplication.isPlaying)
+			return;
+
 		EditorApplication.SaveScene ();
 		EditorApplication.OpenScene("Assets/Scenes/MainMenu.unity");
 		EditorApplication.isPlaying = true;
