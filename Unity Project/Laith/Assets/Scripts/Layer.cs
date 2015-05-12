063ed65fba1e9826a63a9bf25da6eef85613ae52
Ossibo <card-gamer@hotmail.com>
2015-05-12 12:46:36 +0200

    Commit Layer

--- Unity Project/Laith/Assets/Scripts/Layer.cs
+++ Unity Project/Laith/Assets/Scripts/Layer.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 public class Layer : MonoBehaviour {
 
 	public static int players = 8;
-	//public static GameObject[] FindGameObjectsWithLayer (int layer)  {
-		//var goArray = FindObjectsOfType(GameObject);
-		//var goList = new List<GameObject>();
-		//for (int i = 0; i < goArray.Length; ++i) {
-			//if (goArray[i].layer == layer) {
-			//	goList.Add(goArray[i]);
-			//}
-		//}
-		//if (goList.Count == 0) {
-		//	return null;
-		//}
-		//return goList.ToArray();
-	//}
+	public static GameObject[] FindGameObjectsWithLayer (int layer)  {
+		GameObject[] goArray = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+		List<GameObject> goList = new List<GameObject>();
+		for (int i = 0; i < goArray.Length; ++i) {
+			if (goArray[i].layer == layer && goArray[i].transform.parent == null) {
+				goList.Add(goArray[i]);
+			}
+		}
+
+		if (goList.Count == 0) {
+			return null;
+		}
+		return goList.ToArray();
+	}
 }
